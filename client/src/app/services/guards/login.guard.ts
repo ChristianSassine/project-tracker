ae5d9eb9cd41ceea99f401445d9fc5b67a454c0d
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2022-05-28 21:58:08 -0400

    Implemented refresh token

---
+++ client/src/app/services/guards/login.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Paths } from 'src/common/paths';
+import { AuthService } from '../auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isLoggedIn().then(()=>{
+      console.log('Authentication successful');
+      this.router.navigate([Paths.Home]);
+      return false;
+    }).catch(() => {
+      console.log('Failed to authenticate');
+      return true;
+  });
+  }
+  
+}
