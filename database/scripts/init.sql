79e427534905d76d6743d29d6634a986f4da2522
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2022-06-16 01:06:39 -0400

    database initialization script done :boom:

---
+++ database/scripts/init.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+    id bigserial PRIMARY KEY,
+    username text NOT NULL,
+    email text,
+    password text NOT NULL
+);
+
+CREATE TABLE projects (
+    id bigserial PRIMARY KEY,
+    title text NOT NULL
+);
+
+CREATE TABLE users_projects (
+    user_id bigint NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
+    project_id bigint NOT NULL REFERENCES public.projects (id) ON DELETE CASCADE
+);
+
+CREATE TABLE tasks (
+    id bigserial PRIMARY KEY,
+    title text NOT NULL,
+    description text,
+    state text NOT NULL,
+    creation_date timestamp NOT NULL,
+    position bigint NOT NULL,
+    project_id bigint NOT NULL REFERENCES public.projects (id) ON DELETE CASCADE
+);
+
+CREATE TABLE logs (
+    id bigserial PRIMARY KEY,
+    date timestamp NOT NULL,
+    type text NOT NULL,
+    arguments text[],
+    user_id bigint NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
+    project_id bigint NOT NULL REFERENCES public.projects (id) ON DELETE CASCADE
+);
