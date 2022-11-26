d8bf6f4e29b3fef429e09bd2962d61c91526053b
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2022-11-26 04:01:47 -0500

    Removed deprecated feature

--- client/src/app/components/task-info/task-info.component.ts
+++ client/src/app/components/task-info/task-info.component.ts
@@ -5,8 +5,6 @@ import { ProjectTask } from 'src/app/interfaces/project-task';
 import { AuthService } from 'src/app/services/auth.service';
 import { TasksService } from 'src/app/services/tasks.service';
 
-type Comment = { commenter: string; date: Date; content: string };
-
 @Component({
     selector: 'app-task-info',
     templateUrl: './task-info.component.html',
@@ -74,11 +72,6 @@ export class TaskInfoComponent implements OnInit, OnDestroy {
 
     onUpdate() {
         const sentObject = { ...this.task, ...this.form.value };
-
-        if (this.task.state != this.form.value.state) {
-            const updatedStateIndex = 0;
-            this.tasksService.updateTaskState(this.form.value.state, updatedStateIndex, this.task.id);
-        }
         this.tasksService.updateTask(sentObject);
         this.form.markAsPristine();
     }
